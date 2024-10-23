62682dccbd89f4b9025657bf6e648f46254d3642
jayss8 <wicasjaylord@gmail.com>
2024-10-23 21:16:20 +0800

    sgi

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -10,12 +10,12 @@ import reactImg from './react.jpg';
 import nodeImg from './node.jpg';
 
 const toolsData = [
-    { id: 1, name: 'HTML', imgSrc: htmlImg },
-    { id: 2, name: 'CSS', imgSrc: cssImg },
-    { id: 3, name: 'Figma', imgSrc: figmaImg },
-    { id: 4, name: 'MongoDB', imgSrc: mongodbImg },
-    { id: 5, name: 'React', imgSrc: reactImg },
-    { id: 6, name: 'Node.js', imgSrc: nodeImg },
+    { id: 1, name: 'HTML', imgSrc: htmlImg, description: 'Structure your web content with HTML.' },
+    { id: 2, name: 'CSS', imgSrc: cssImg, description: 'Style your web pages beautifully with CSS.' },
+    { id: 3, name: 'Figma', imgSrc: figmaImg, description: 'Design and prototype user interfaces with Figma.' },
+    { id: 4, name: 'MongoDB', imgSrc: mongodbImg, description: 'NoSQL database for storing data.' },
+    { id: 5, name: 'React', imgSrc: reactImg, description: 'A JavaScript library for building user interfaces.' },
+    { id: 6, name: 'Node.js', imgSrc: nodeImg, description: 'JavaScript runtime for server-side development.' },
 ];
 
 const Tools = () => {
@@ -27,6 +27,7 @@ const Tools = () => {
                     <div className="tool-item" key={tool.id}>
                         <img src={tool.imgSrc} alt={tool.name} />
                         <h3>{tool.name}</h3>
+                        <p>{tool.description}</p> {/* Displaying the tool description */}
                     </div>
                 ))}
             </div>
